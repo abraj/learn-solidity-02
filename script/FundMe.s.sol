2cb5f6e319c4de699df64569123a1e5200011f9c
Abhishek Raj <abhi@raj.me>
2024-09-20 23:25:55 +0530

    Update contract address

--- script/FundMe.s.sol
+++ script/FundMe.s.sol
@@ -24,7 +24,7 @@ contract FundFundMe is Script {
 
   function run() external {
     // address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
-    address mostRecentDeployed = address(0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9);
+    address mostRecentDeployed = address(0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512);
 
     fundFundMe(mostRecentDeployed);
   }
@@ -39,7 +39,7 @@ contract FundFundMe is Script {
 contract WithdrawFundMe is Script {
   function run() external {
     // address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
-    address mostRecentDeployed = address(0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9);
+    address mostRecentDeployed = address(0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512);
 
     withdrawFundMe(mostRecentDeployed);
   }
